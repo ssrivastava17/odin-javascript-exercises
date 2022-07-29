9734f5ddf9a86abfbc88f101b85854a1dd525c33
Stephanie Srivastava <seichman2011@gmail.com>
2022-07-29 09:02:27 -0400

    exercise 4,5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    let newArray = [];
 
+    for(let i = 0; i < array.length; i++){
+        newArray.push(array[i]);
+        for (const arg of theArgs){
+            if(array[i] === arg){
+                newArray.pop()
+                break;
+            }else{
+                continue
+            }
+        }
+        
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    if(value1 < 0 || value2 < 0 || !Number.isInteger(value1) || !Number.isInteger(value2)){
+        return 'ERROR'
+    }
+    let first = null;
+    let last = null;
 
+    if(value1 < value2){
+        first = value1;
+        last = value2;
+    }else{
+        first = value2;
+        last = value1;
+    }
+    
+    let sum = 0;
+    for(let i = first; i <= last; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
