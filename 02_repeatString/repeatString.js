366026bc1fd66a6081a3705d9510ac65e3c4b18f
Stephanie Srivastava <seichman2011@gmail.com>
2022-07-28 21:48:02 -0400

    exercises 1,2,3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let myString = "";
+    for(let i = 0; i < num; i++){
+        myString += string;
+    }
+    return myString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    let position = 0;
+    for(let i=string.length-1; i>=0; i--){
+        newString += string[i];
+        position++;
+    }
+    return newString;
 };
 
 // Do not edit below this line
